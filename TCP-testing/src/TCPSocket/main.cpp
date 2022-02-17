a264276e96fb0277023653d523007b50307e27f1
lgad31vn <lgad31vn@gmail.com>
2022-02-17 17:38:28 -0500

    init TCP-testing

---
+++ TCP-testing/src/TCPSocket/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+
+
+int main() {
+
+    // Create a socket
+    // Bind the socket to IP port
+    // Mark the socket for listening in
+    // Accept a call
+    //Close the listen socket
+    //While receiving display messace, echo message
+    // Close socket
+
+    return 0;
+}
