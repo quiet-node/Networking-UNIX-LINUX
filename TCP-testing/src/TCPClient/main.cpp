695fc83571a5ac7a8614dee13b2564600bf6a34d
lgad31vn <lgad31vn@gmail.com>
2022-02-18 18:56:55 -0500

    added client

---
+++ TCP-testing/src/TCPClient/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    //	Create a socket
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0); // like communication line to the server
+    if (clientSocket == -1)
+    {
+        return 1;
+    }
+
+    //	Create a hint structure for the server we're connecting with
+    int port = 54000;
+    string ipAddress = "127.0.0.1";
+
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(port);
+    inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+    //	Connect to the server on the socket
+    int connectRes = connect(clientSocket, (sockaddr*)&hint, sizeof(hint));
+    if (connectRes == -1)
+    {
+        return 1;
+    }
+
+    //	While loop:
+    char buf[4096];
+    string userInput;
+
+
+    do {
+        //		Enter lines of text
+        cout << "$>> ";
+        getline(cin, userInput);
+
+        //		Send to server
+        int sendRes = send(clientSocket, userInput.c_str(), userInput.size() + 1, 0);
+        if (sendRes == -1)
+        {
+            cout << "Could not send to server! Whoops!\r\n";
+            continue;
+        }
+
+        //		Wait for response
+        memset(buf, 0, 4096);
+        int bytesReceived = recv(clientSocket, buf, 4096, 0);
+        if (bytesReceived == -1)
+        {
+            cout << "There was an error getting response from server\r\n";
+        }
+        else
+        {
+            //		Display response
+            cout << "SERVER> " << string(buf, bytesReceived) << "\r\n";
+        }
+    } while(true);
+
+    //	Close the socket
+    close(clientSocket );
+
+    return 0;
+}
